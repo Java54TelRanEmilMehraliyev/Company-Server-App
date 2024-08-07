53e1145f7b9272148e6e57e2af129643a8aefc56
user.emial <emil.mehraliyev003@gmail.com>
2024-08-07 22:39:44 +0300

    method getResponse in Protocol was upgraded to Multithreadable with adding synchronized to method title

--- src/main/java/telran/CompanyServerAppl.java
+++ src/main/java/telran/CompanyServerAppl.java
@@ -1,48 +1,52 @@
 package telran;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.util.Scanner;
 
 import telran.employees.*;
 import telran.io.Persistable;
 import telran.net.Protocol;
 import telran.net.TcpServer;
 
-public class CompanyServerAppl {
+import static telran.net.TcpConfigurationProperties.*
+;public class CompanyServerAppl {
 
-    private static final String FILE_NAME = "employeesTest.data";
-    private static final int PORT = 8000;
+	private static final String FILE_NAME = "employeesTest.data";
+	private static final int PORT = 5000;
+	
+	public static void main(String[] args) {
+		
+		Company company = new CompanyMapsImpl();
+		Persistable persistable = null;
+		if(company instanceof Persistable) {
+			persistable = (Persistable)company;
+		}
+		if (persistable != null) {
+			try {
+				persistable.restore(FILE_NAME);
+			} catch (Exception e) {
 
-    public static void main(String[] args) {
-        Company company = new CompanyMapsImpl();
-        File file = new File(FILE_NAME);
-        if (file.exists()) {
-            try {
-                ((Persistable)company).restore(FILE_NAME);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+			} 
+		}
+		Protocol protocol = new CompanyProtocol(company);
+		TcpServer tcpServer = new TcpServer(protocol, PORT);
+		Thread serverThread = new Thread(tcpServer);
+		serverThread.start();
+		boolean running = true;
+		try (Scanner scanner = new Scanner(System.in);){
+			while(running) {
+				System.out.println(String.format("Enter %s for the server shutdown", SHUTDOWN));
+				String line = scanner.nextLine();
+				if (line != null && line.equals(SHUTDOWN)) {
+					running = false;
+					tcpServer.shutdown();
+					if (persistable != null) {
+						persistable.save(FILE_NAME);
+					}
+				}
+			}
+		}
 
-        Protocol protocol = new CompanyProtocol(company);
-        TcpServer tcpServer = new TcpServer(protocol, PORT);
+	}
 
-        Thread serverThread = new Thread(() -> tcpServer.run());
-        serverThread.start();
-
-        try (BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))) {
-            String command;
-            while (!(command = consoleReader.readLine()).equalsIgnoreCase("shutdown")) {
-                System.out.println("Введите 'shutdown' для остановки сервера.");
-            }
-            tcpServer.shutdown();
-            serverThread.join();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            ((Persistable)company).save(FILE_NAME);
-        }
-    }
 }
